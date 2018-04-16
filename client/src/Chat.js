73695ca070920a7eba8a6489c55d58a32eda9f72
kiwiyan <yanboyiha@163.com>
2018-04-16 22:34:44 +0800

    feat: 添加聊天组件

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -6,17 +6,47 @@ class TodoList extends PureComponent {
     constructor(props) {
         super(props);
         this.state = {
-            value: ''
+            inputValue: '',
+            talkLists: ['system: 1 user is online','jack: where is my rose~']
         };
-        ['handleClick'].forEach(func => {
+        ['handleChange', 'sengMsg'].forEach(func => {
             this[func] = this[func].bind(this);
         })
     }
-    handleClick() {
+    handleChange(e) {
+        this.setState({
+            inputValue: e.target.value
+        })
+    }
+    sengMsg() {
+        this.setState({
+            talkLists: [...this.state.talkLists, this.state.inputValue],
+            inputValue: ''
+        })
     }
     render() {
         return(
-            <div className="test"><div className="t2">some component</div></div>
+            <article className="wrap">
+                <header>this is header</header>
+                <section>
+                    <div className="talk-content">
+                        <ul>
+                        {
+                            this.state.talkLists.map(val => {
+                                return (
+                                    <li>{val}</li>
+                                )
+                            })
+                        }
+                        </ul>
+                    </div>
+                    <div className="send-content">
+                        <input type="text" value={this.state.inputValue} onChange={this.handleChange}/>
+                        <button onClick={this.sengMsg}>send</button>
+                    </div>
+                </section>
+                <footer>this is footer</footer>
+            </article>
         )
     }
 }
